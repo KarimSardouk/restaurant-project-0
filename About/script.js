6e687e752507fab1b33593f611b3e8e7e17a3240
Assala Meslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-24 10:09:22 +0300

    new changes

---
+++ About/script.js
@@ -0,0 +1,30 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+  .bindPopup('Hello, I am a marker!')
+  .openPopup();
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+    accordionHeaders.forEach(header => {
+        header.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});

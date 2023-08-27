dbd5e0be6a4dffd926a8ca05ff015717dbed9f14
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-27 22:59:52 +0300

    anythin

---
+++ script.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" current", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " current";
+
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds (2000 milliseconds)
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
